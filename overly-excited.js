0e0c4842f767ab3dd0b0525610946eda64535787
ben harrington <harringtonben@icloud.com>
2017-08-02 11:37:02 -0500

    getting uncaught syntax error when calling function

--- overly-excited.js
+++ overly-excited.js
@@ -1,30 +1,16 @@
 // Create an array that contains the words in the sentence
 var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
-console.log("fuck this project");
-
-
  
 // The addExcitement function should accept the array as the sole argument
-function addExcitement() {
-		
-		for(var i = 0; i < sentence.height; i++) {
-    	var words = sentence.height-(i+1);
-    	console.log();
+function addExcitement(sentence) {
+	for (var i = 0; i <= sentence.length; i++) {
+        var runningSentence = sentence[i];
+        runningSentence += sentence[i];
+        console.log(runningSentence);
     }
 
-function generatePyramid() {
-    var totalNumberofRows = sentence.length;
-    for (var i = 1; i <= totalNumberofRows; i++) {
-        for (var j = 1; j <= i; j++) {
 
-            sentence.push(j);
-            console.log(j);
-        }
-        console.log("\n");
-    }
-}
+// Invoke the function and pass in the array
 
-//  // Invoke the function and pass in the array
 addExcitement(sentence);
-console.log(sentence);

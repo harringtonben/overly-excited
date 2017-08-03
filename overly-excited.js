76af0c3e35fc22b3ecbf1988d6a610cdf24518a1
ben harrington <harringtonben@icloud.com>
2017-08-02 21:08:08 -0500

    finished part of overly excited exercise

--- overly-excited.js
+++ overly-excited.js
@@ -1,16 +1,21 @@
 // Create an array that contains the words in the sentence
 var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
-
- 
+var sentenceContainer = [];
 // The addExcitement function should accept the array as the sole argument
 function addExcitement(sentence) {
-	for (var i = 0; i <= sentence.length; i++) {
-        var runningSentence = sentence[i];
-        runningSentence += sentence[i];
-        console.log(runningSentence);
+    // debugger;
+    var totalNumberofRows = sentence.length,
+        arr;
+    for (var i = 0; i < totalNumberofRows; i++) {
+        arr = [];
+        for (var j = 0; j <= i; j++) {
+            arr.push(sentence[j]); 
     }
-
+        console.log(arr.join(" ") + "\n");
+    }
+}
 
 // Invoke the function and pass in the array
 
 addExcitement(sentence);
+
